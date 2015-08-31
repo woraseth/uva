5ca652dd2643c9f52f4355381c9338a9a7894e1f
woraseth <woraseth@users.noreply.github.com>
2015-08-31 18:05:30 +0700

    Update P10026.java

--- P10026.java
+++ P10026.java
@@ -39,19 +39,21 @@ public class Main {
         if (a > b) {
           return -1;
         } else if (a == b) {
-          if (o1.day > o2.day) {
-            return 1;
-          } else if (o1.day == o2.day) {
-            return 0;
-          } else {
-            return -1;
-          }
+          return 0;
+          // unnecessary
+//          if (o1.day > o2.day) {
+//            return 1;
+//          } else if (o1.day == o2.day) {
+//            return 0;
+//          } else {
+//            return -1;
+//          }
         } else {
           return 1;
         }
       }
     });
-    System.out.println(job);
+//    System.out.println(job);
     for (int i = 0; i < job.size(); i++) {
       if (i != 0) {
         System.out.print(" ");
