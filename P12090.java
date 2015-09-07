2d87abf137c7de6930f18afc8502394dd24fa171
woraseth <woraseth@users.noreply.github.com>
2015-09-08 01:24:25 +0700

    Update P12090.java

--- P12090.java
+++ P12090.java
@@ -1,8 +1,8 @@
 // 12090 - Counting Zeroes
-// after discussing with Ake
+// don't forget to check if cannot factor (base.size() == 0)
 
+import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,14 +10,15 @@ public class Main {
 
   static Scanner sc = new Scanner(System.in);
 
-  static boolean[] isPrime;
+  static boolean[] nonPrime;
+  static int[] prime = new int[5761455];   // 5761455 primes less than 100M
+  static int primeCount;
   static List<Integer> base;
   static List<Integer> power;
   static List<Integer> factor;
   static int sum = 0;
 
   static void allFactor(long n, int index, long f) {
-//    System.out.println("hhh");
     if (index < power.size()) {
       for (int i = 0; i <= power.get(index); i++) {
         long pk = 1;
@@ -42,35 +43,33 @@ public class Main {
   static void factor(long n) {
     base = new ArrayList<>();
     power = new ArrayList<>();
-    for (int i = 2; i < isPrime.length; i++) {
-//      System.out.println(n);
-      if (i > n) {
+    for (int i = 0; i < primeCount; i++) {
+      int p = prime[i];
+      if (p > n) {
         break;
       }
-      if (isPrime[i]) {
-        if (n % i == 0) {
-          int count = 0;
-          while (n % i == 0) {
-            n /= i;
-            count++;
-          }
-          base.add(i);
-          power.add(count);
-//          System.out.printf("%d^%d%n", i, count);
+
+      if (n % p == 0) {
+        int count = 0;
+        while (n % p == 0) {
+          n /= p;
+          count++;
         }
+        base.add(p);
+        power.add(count);
+//        System.out.printf("%d^%d%n", p, count);
       }
     }
   }
 
   static void seive() {
-    int max = 10 * 1000 * 1000;
-    isPrime = new boolean[max];
-    Arrays.fill(isPrime, 0, max, true);
-    for (int i = 2; i < isPrime.length; i++) {
-      if (isPrime[i]) {
-//        System.out.println(i);
-        for (int j = i + i; j < isPrime.length; j += i) {
-          isPrime[j] = false;
+    int max = 50 * 1000 * 1000;
+    nonPrime = new boolean[max];
+    for (int i = 2; i < nonPrime.length; i++) {
+      if (!nonPrime[i]) {
+        prime[primeCount++] = i;
+        for (int j = i + i; j < nonPrime.length; j += i) {
+          nonPrime[j] = true;
         }
       }
     }
@@ -80,13 +79,27 @@ public class Main {
     seive();
     while (true) {
       long n = sc.nextLong();
+
       if (n == 0) {
         break;
       }
+      
+      // test prime
+      BigInteger b = new BigInteger(n + "");
+      if (b.isProbablePrime(128)) {  // 2714901685147 is prime
+        System.out.printf("%s 1%n", b.toString());
+        continue;
+      }
+
       factor(n);
-      sum = 0;
-      allFactor(n, 0, 1);
-      System.out.printf("%d %d%n", n, sum);
+      if (base.size() != 0) {
+        sum = 0;
+        allFactor(n, 0, 1);
+        System.out.printf("%d %d%n", n, sum);
+      } else {
+        System.out.printf("%d 1%n", n);
+      }
+
     }
   }
 }
