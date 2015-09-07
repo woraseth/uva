153e37f97f0ff054213642b15cd805e9d8d852ae
woraseth <woraseth@users.noreply.github.com>
2015-09-07 23:35:23 +0700

    Create Sieve.java

---
+++ Sieve.java
@@ -0,0 +1,28 @@
+// seive
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+  static Scanner sc = new Scanner(System.in);
+
+  static boolean[] isPrime;
+
+  static void seive(int n) {
+    isPrime = new boolean[n];
+    Arrays.fill(isPrime, 0, n, true);
+    for (int i = 2; i < isPrime.length; i++) {
+      if (isPrime[i]) {
+        System.out.println(i);
+        for (int j = i + i; j < isPrime.length; j += i) {
+          isPrime[j] = false;
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    seive(50);  
+  }
+}
