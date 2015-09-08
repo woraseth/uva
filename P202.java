aa3e2a3a18bd2270d974686f77d304fac08757fe
woraseth <woraseth@users.noreply.github.com>
2015-09-09 02:24:15 +0700

    Update P202.java

--- P202.java
+++ P202.java
@@ -1,5 +1,3 @@
-// cycle occurs when dividing with the same number
-
 //1/397 = 0.(00251889168765743073047858942065491183879093198992...)
 //   99 = number of digits in repeating cycle
 
@@ -14,58 +12,62 @@ import java.util.Scanner;
 public class Main {
 
   static Scanner sc = new Scanner(System.in);
+  static List<Integer> result;
+  static List<Integer> list;
+  static int cycle;
+  static int intPart;
+
+  static int process(int n, int d) {
+    result = new ArrayList<>();
+    list = new ArrayList<>();
+    cycle = -1;
+    intPart = n / d;
+
+    n = n % d;
+
+    int t;
+    while (true) {
+      t = n * 10;
+      n = t % d;
+      if (!list.contains(t)) {
+        result.add(t / d);
+        list.add(t);
+      } else {
+        cycle = list.size() - list.indexOf(t);
+        return list.indexOf(t);
+      }
+    }
+  }
+
+  static void output(int n, int d, int listIndex) {
+    StringBuilder sb = new StringBuilder();
+    if (listIndex + 1 + cycle <= 50) {
+      for (int i : result) {
+        sb.append(i);
+      }
+      sb.insert(listIndex, "(");
+      sb.append(")");
+      sb.insert(0, intPart + ".");
+    } else {
+      for (int i = 0; i < 50; i++) {
+        sb.append(result.get(i));
+      }
+      sb.insert(listIndex, "(");
+      sb.append("...)");
+      sb.insert(0, intPart + ".");
+    }
+    System.out.printf("%d/%d = %s%n", n, d, sb);
+    System.out.printf("   %d = number of digits in repeating cycle%n%n", cycle);
+
+  }
 
   public static void main(String[] args) {
     while (sc.hasNextInt()) {
       int n = sc.nextInt();
-      final int d = sc.nextInt();
-//    int n = 5;
-      final int m = n;
-//    final int d = 43;
-      List<Integer> result = new ArrayList<>();
-      List<Integer> list = new ArrayList<>();
-      int cycle = -1;
-      int intPart = n / d;
-      n = n % d;
+      int d = sc.nextInt();
 
-      int t;
-      while (true) {
-        int a;
-        t = n * 10;
-
-        a = t / d;
-        n = t % d;
-//      System.out.println(list);
-        if (!list.contains(t)) {
-          result.add(a);
-          list.add(t);
-        } else {
-          cycle = list.size() - list.indexOf(t);
-          break;
-        }
-
-      }
-//      System.out.println(cycle);
-      StringBuilder sb = new StringBuilder();
-      if (list.indexOf(t) + 1 + cycle <= 50) {
-        for (int i : result) {
-          sb.append(i);
-        }
-//      sb.insert(list.indexOf(n) + cycle, ")");
-        sb.insert(list.indexOf(t), "(");
-        sb.append(")");
-        sb.insert(0, intPart + ".");
-      } else {
-        for (int i = 0; i < 50; i++) {
-          sb.append(result.get(i));
-        }
-        sb.insert(list.indexOf(t), "(");
-        sb.append("...)");
-        sb.insert(0, intPart + ".");
-      }
-      System.out.printf("%d/%d = %s%n", m, d, sb);
-      System.out.printf("   %d = number of digits in repeating cycle%n%n", cycle);
-
+      int listIndex = process(n, d);
+      output(n, d, listIndex);
     }
   }
 }
