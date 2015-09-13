92464a9c0106637edc9925ba374c90cd323e9ff6
woraseth <woraseth@users.noreply.github.com>
2015-09-14 06:18:43 +0700

    Update P507.java

--- P507.java
+++ P507.java
@@ -13,28 +13,28 @@ public class Main {
     int[] count = new int[a.length];
     for (int i = 1; i < sum.length; i++) {
       int s = sum[i - 1] + a[i];
-      sum[i] = s < 0 ? 0 : s;
-      count[i] = sum[i] == 0 ? 0 : count[i - 1] + 1;
+      sum[i] = s <= 0 ? 0 : s;
+      count[i] = sum[i] <= 0 ? 0 : count[i - 1] + 1;
     }
 //    System.out.println("------");
 //    System.out.println(java.util.Arrays.toString(sum));
 //    System.out.println(java.util.Arrays.toString(count));
     int max = Integer.MIN_VALUE;
     int maxIndex = -1;
-    for (int i = sum.length - 1; i >= 0; i--) {
+    for (int i = sum.length - 1; i >= 1; i--) {
 //    for (int i = 0; i < sum.length; i++) {
       if (max < sum[i]) {
         max = sum[i];
         maxIndex = i;
       } else if (max == sum[i]) {
-        if (count[i] >= count[maxIndex]) {
+        if (count[maxIndex] <= count[i]) {
           maxIndex = i;
         }
       }
     }
 
     int zeroIndex = maxIndex - count[maxIndex];
-    if (sum[maxIndex] == 0) {
+    if (sum[maxIndex] <= 0) {
       System.out.printf("Route %d has no nice parts%n", t + 1);
     } else {
       System.out.printf("The nicest part of route %d is between stops %d and %d%n", t + 1, zeroIndex + 1, maxIndex + 1);
