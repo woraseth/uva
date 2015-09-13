796377d23b8482f56ae6560e4eb4d9f6052d9bd0
woraseth <woraseth@users.noreply.github.com>
2015-09-13 10:14:25 +0700

    Create P10079.java

---
+++ P10079.java
@@ -0,0 +1,28 @@
+// 10079 – Pizza Cutting
+// return long
+
+import java.util.Scanner;
+
+public class Main {
+  static long process(int n) {
+    if (n == 0) return 1;
+    long x0 = 2;
+    for (int i = 2; i <= n; i++) {
+      x0 += i;
+    }
+    return x0;
+  }
+
+  static Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+    while (sc.hasNextInt()) {
+      int n = sc.nextInt();
+      if (n < 0)
+        break;
+      
+      System.out.println(process(n));
+    }
+  }
+}
